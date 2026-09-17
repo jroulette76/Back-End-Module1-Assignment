05f9361e51b36e84aaf0a5505102dd30844a172c
Julian Roulette <jroulette76@academic.rrc.ca>
2026-09-17 13:55:51 -0500

    Feat: add second function test for calculatePortfolioPerformance

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -2,7 +2,7 @@ import { describe } from "node:test";
 import { calculatePortfolioPerformance } from "../src/portfolio/portfolioPerformance";
 
 describe("calculatePortfolioPerformance", () => {
-    it("should calculate a significant gain correctly", () => {
+    it("should return Excellent performance for a significant gain", () => {
         // Arrange
         const initialInvestment = 10000;
         const currentValue = 15000;
@@ -14,5 +14,19 @@ describe("calculatePortfolioPerformance", () => {
         expect(result.profitOrLoss).toBe(5000);
         expect(result.percentageChange).toBe(50);
         expect(result.performanceSummary).toBe("Excellent performance! Your investments are doing great.")
+    });
+
+    it("should return a Solid gain for a 20% increase", () => {
+        // Arrange
+        const initialInvestment = 10000;
+        const currentValue = 12000;
+
+        // Act
+        const result = calculatePortfolioPerformance(initialInvestment, currentValue);
+        
+        // Assert
+        expect(result.profitOrLoss).toBe(2000);
+        expect(result.percentageChange).toBe(20);
+        expect(result.performanceSummary).toBe("Solid gain. Keep monitoring your investments.")
     });
 });
