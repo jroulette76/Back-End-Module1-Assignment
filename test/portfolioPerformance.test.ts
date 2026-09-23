0e573034bafd977a19dcd1393ebe61d7bcab5e0a
Julian Roulette <jroulette76@academic.rrc.ca>
2026-09-23 09:36:49 -0500

    Feat: changed the four tests to match the video demo.

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -1,45 +1,45 @@
 import { calculatePortfolioPerformance } from "../src/portfolio/portfolioPerformance";
 
 describe("calculatePortfolioPerformance", () => {
-    it("should return Excellent performance for a significant gain", () => {
+    it("should return Excellent performance for a gain more than 30%.", () => {
         // Arrange
         const initialInvestment = 10000;
-        const currentValue = 15000;
+        const currentValue = 16000;
 
         // Act
         const result = calculatePortfolioPerformance(initialInvestment, currentValue);
 
         // Assert
-        expect(result.profitOrLoss).toBe(5000);
-        expect(result.percentageChange).toBe(50);
+        expect(result.profitOrLoss).toBe(6000);
+        expect(result.percentageChange).toBe(60);
         expect(result.performanceSummary).toBe("Excellent performance! Your investments are doing great.")
     });
 
-    it("should return a Solid gain for a 20% increase", () => {
+    it("should return a Solid gain for a 29.99% increase", () => {
         // Arrange
         const initialInvestment = 10000;
-        const currentValue = 12000;
+        const currentValue = 12999.9;
 
         // Act
         const result = calculatePortfolioPerformance(initialInvestment, currentValue);
         
         // Assert
-        expect(result.profitOrLoss).toBe(2000);
-        expect(result.percentageChange).toBe(20);
+        expect(result.profitOrLoss).toBe(2999.8999999999996);
+        expect(result.percentageChange).toBe(29.999);
         expect(result.performanceSummary).toBe("Solid gain. Keep monitoring your investments.")
     });
 
-    it("should return a Modest gain for a 5% increase", () => {
+    it("should return a Modest gain for a 9.99% increase", () => {
         // Arrange
         const initialInvestment = 10000;
-        const currentValue = 10500;
+        const currentValue = 10999.9;
 
         // Act
         const result = calculatePortfolioPerformance(initialInvestment, currentValue);
         
         // Assert
-        expect(result.profitOrLoss).toBe(500);
-        expect(result.percentageChange).toBe(5);
+        expect(result.profitOrLoss).toBe(999.8999999999996);
+        expect(result.percentageChange).toBe(9.998999999999997);
         expect(result.performanceSummary).toBe("Modest gain. Your portfolio is growing slowly.")
     });
 
